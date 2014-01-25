1490d142f010e9aa984a2385a7b959aee3ce6189
jaldag <jchristophera@hotmail.com>
2014-01-25 00:35:39 -0800

    Controller support in!

--- js/main.js
+++ js/main.js
@@ -1,28 +1,42 @@
 // Start enchant
 enchant();
+var controller = null;
+function updateController()
+{
+	if (navigator.webkitGetGamepads) {
+		controller = navigator.webkitGetGamepads()[0];
+	}
+}
 
 // When document loads, set up basic game
 window.onload = function() {
 	var game = new Game(600, 720);
 	game.preload('images/bg1.png');
-
+	
 	game.fps = 30;
 	game.scale = 1;
+	var lab1 = new Label("Hello there!");
+	lab1.y = 50;
+	lab1.color = 'white';
 	game.onload = function() {
 		console.log("sup guys, I'm your game");
 
-		var scene, label, bg;
-		scene = new Scene();
+		var label, bg;
 		label = new Label("FIVE Players.  ONE Controller.");
 		label.color = 'white';
 		bg = new Sprite(600, 720);
 		bg.image = game.assets['images/bg1.png'];
 
-		scene.addChild(bg);
-		scene.addChild(label);
-
-		game.pushScene(scene);
+		game.rootScene.addChild(bg);
+		game.rootScene.addChild(label);
+		game.rootScene.addChild(lab1);
 	}
 
+	game.rootScene.addEventListener('enterframe', function(e) {
+		updateController();
+		if (controller && controller.buttons[0] == 1) {
+			lab1.text = "Button pushed!";
+		}
+	});
 	game.start();
 }
