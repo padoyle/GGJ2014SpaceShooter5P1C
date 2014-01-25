cfd4750fdda38f580f937d0c19fac77f82cc7240
Paul Doyle <padoyle@calpoly.edu>
2014-01-25 03:29:49 -0800

    System in place for defining enemy behavior

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 // Start enchant
 enchant();
+//Global variables. Don't change in code
+var Controller_A = 0;
+var Controller_B = 1;
+var Controller_X = 2;
+var Controller_Y = 3;
+var Controller_Left_Bump = 4;
+var Controller_Right_Bump = 5;
+var Controller_Left_Trigger = 6;
+var Controller_Right_Trigger = 7;
+var Controller_Back = 8;
+var Controller_Start = 9;
+var Controller_Left_Stick = 10;
+var Controller_Right_Stick = 11;
+var Controller_Up_Dpad = 12;
+var Controller_Down_Dpad = 13;
+var Controller_Left_Dpad = 14;
+var Controller_Right_Dpad = 15;
+var Controller_Left_X_Axis = 0;
+var Controller_Left_Y_Axis = 1;
+var Controller_Right_X_Axis = 2;
+var Controller_Right_Y_Axis = 3;
+
+var gameWidth = 600;
+var gameHeight = 720;
+
 var controller = null;
 function updateController()
 {
@@ -10,37 +35,172 @@ function updateController()
 
 // When document loads, set up basic game
 window.onload = function() {
-	var game = new Game(600, 720);
-	game.preload('images/bg1.png');
+	var game = new Game(gameWidth, gameHeight);
+	game.preload('images/bg1.png', 'images/Square.png');
 	
-	game.fps = 30;
+	game.fps = 60;
 	game.scale = 1;
-	var lab1 = new Label("Hello there!");
-	lab1.y = 50;
-	lab1.color = 'white';
+
 	game.onload = function() {
 		console.log("sup guys, I'm your game");
 
 		var label, bg;
 		label = new Label("FIVE Players.  ONE Controller.");
 		label.color = 'white';
-		bg = new Sprite(600, 720);
+		bg = new Sprite(gameWidth, gameHeight);
 		bg.image = game.assets['images/bg1.png'];
 
 		game.rootScene.addChild(bg);
 		game.rootScene.addChild(label);
-		game.rootScene.addChild(lab1);
+		
+		Ship = Class.create(Sprite, {
+			initialize: function() {
+				Sprite.call(this, 30, 30);
+				this.image = game.assets['images/Square.png'];
+				this.frame = 0;
+				this.x = 300;
+				this.y = 360;
+				this.addEventListener('enterframe', function() {
+					if (this.y < 0) {
+						this.y = 0;
+					}	
+					if (this.y > gameWidth - this.height) {
+						this.y = gameWidth - this.height;
+					}
+					if (this.x < 0) {
+						this.x = 0;
+					}
+					if (this.x > gameWidth - this.width) {
+						this.x = gameWidth - this.width;
+					}
+				});
+			}
+		});
+		var ship = new Ship();
+		game.rootScene.addChild(ship);
+		
+		var Enemy = Class.create(Sprite, {
+			initialize: function(width, height) {
+				Sprite.call(this, width, height);
+				this.frame = 0;
+				this.velY = 0;
+				this.velX = 0;
+			},
+			onenterframe: function() {
+				if (this.y > gameHeight - this.height) {
+					this.y = gameHeight - this.height;
+					this.velY *= -1;
+				}
+				if (this.y < 0) {
+					this.y = 0;
+					this.velY *= -1;
+				}
+				if (this.x < 0) {
+					this.x = 0;
+					this.velX *= -1;
+				}
+				if (this.x > gameWidth - this.width) {
+					this.x = gameWidth - this.width;
+					this.velX *= -1;
+				}
+				this.x += this.velX;
+				this.y += this.velY;
+			}
+		});
+		var EnemyX = Class.create(Enemy, {
+			initialize: function() {
+				Enemy.call(this, 30, 30);
+				this.image = game.assets['images/Square.png'];
+				this.x = 30;
+				this.y = 50;
+				this.velX = 1;
+				this.velY = 3;
+			}
+		});
+		var EnemyY = Class.create(Enemy, {
+			initialize: function() {
+				Enemy.call(this, 30, 30);
+				this.image = game.assets['images/Square.png'];
+				this.x = 500;
+				this.y = 50;
+				this.velX = 1;
+				this.velY = 3;
+			}
+		});
 
-		var move = new Move(1, 2, 3, 4, 5);
-		console.log(move.direction, move.speed);
+		var EnemyWithMoves = Class.create(Enemy, {
+			initialize: function(_moveset) {
+				Enemy.call(this, 30, 30);
+				this.image = game.assets['images/Square.png'];
+				this.moveset = _moveset;
+				this.move = this.moveset.nextMove();
+				this.move_progress = 0;
+				this.x = 300;
+				this.y = 50;
+				this.velX = Math.cos(this.move.direction) * this.move.speed;
+				this.velY = Math.sin(this.move.direction) * this.move.speed;
+				console.log("Change move!");
+			},
+
+			onenterframe: function() {
+				if (this.move_progress >= this.move.duration) {
+					this.move = this.moveset.nextMove();
+					this.move_progress = 0;
+					this.velX = Math.cos(this.move.direction) * this.move.speed;
+					this.velY = Math.sin(this.move.direction) * this.move.speed;
+					console.log("Change move!");
+				}
+
+				if (this.y > gameHeight - this.height) {
+					this.y = gameHeight - this.height;
+				}
+				if (this.y < 0) {
+					this.y = 0;
+				}
+				if (this.x < 0) {
+					this.x = 0;
+				}
+				if (this.x > gameWidth - this.width) {
+					this.x = gameWidth - this.width;
+				}
+				this.x += this.velX;
+				this.y += this.velY;
+
+				this.move_progress++;
+			}
+		});
+		
+		var enemyX = new EnemyX();
+		var enemyY = new EnemyY();
+
+		var enemyWithMoves = new EnemyWithMoves(new MoveSet(new Array(
+				new Move(0, 1.5, 60, 0, 0),
+				new Move((11.0 / 4.0) * Math.PI, 3, 30, 0, 0),
+				new Move((13.0 / 4.0) * Math.PI, 4, 30, 0, 0),
+				new Move((1.0 / 2.0) * Math.PI, 2, 60, 0, 0))));
+		
+		game.rootScene.addChild(enemyX);
+		game.rootScene.addChild(enemyY);
+		game.rootScene.addChild(enemyWithMoves);		
+		
+		game.rootScene.addEventListener('enterframe', function(e) {
+			updateController();
+			if (controller) {
+				if (controller.axes[Controller_Left_X_Axis] > 0.1 || controller.axes[Controller_Left_X_Axis] < -0.1) {
+					ship.x += controller.axes[Controller_Left_X_Axis];
+				}
+				if (controller.axes[Controller_Left_Y_Axis] > 0.1 || controller.axes[Controller_Left_Y_Axis] < -0.1) {
+					ship.y += controller.axes[Controller_Left_Y_Axis];
+				}
+				if (controller.buttons[Controller_A] === 1) {
+					ship.scaleX += .1;
+				}
+				if (controller.buttons[Controller_B] === 1) {
+					ship.scaleX -= .1;
+				}
+			}
+		});
 	}
-
-	game.rootScene.addEventListener('enterframe', function(e) {
-		updateController();
-		if (controller && controller.buttons[0] == 1) {
-			lab1.text = "Button pushed!";
-		}
-	});
 
 	game.start();
 }
@@ -59,14 +219,14 @@ var MoveSet = Class.create({
 	initialize: function(_moves, _repeat) {
 		this.moves = _moves;
 		this.repeat = _repeat;
-		this.current = 0;
+		this.current = -1;
 		this.total = _moves.length;
-	}
+	},
 
 	nextMove: function() {
-		current++;
-		if (current >= total)
-			current = 0;
-		return moves[current];
+		this.current++;
+		if (this.current >= this.total)
+			this.current = 0;
+		return this.moves[this.current];
 	}
 });
